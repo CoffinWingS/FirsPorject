15c7b99e3ae3fbabe7aec632e195a58fa8e299d7
CoffinWingS <156872580+CoffinWingS@users.noreply.github.com>
2025-09-26 03:20:23 +0700

    Update Home.py

--- pages/Home.py
+++ pages/Home.py
@@ -1,16 +1,18 @@
 import streamlit as st
+from KNN import run_knn
+from DForest import run_dforest
+from Bay import run_bay
 
 st.title("Customer Purchase Prediction System")
-st.write("ยินดีต้อนรับ! โปรดเลือกโมเดลสำหรับพยากรณ์การซื้อของลูกค้า:")
+st.sidebar.title("เลือกโมเดล")
+model_choice = st.sidebar.radio(
+    "เลือกโมเดลที่ต้องการใช้",
+    ("KNN", "Random Forest", "Naive Bayes")
+)
 
-st.sidebar.title("เลือกหน้า")
-page = st.sidebar.selectbox("ไปยังโมเดล", ["Home", "KNN", "Random Forest", "Naive Bayes"])
-
-if page == "Home":
-    st.write("นี่คือหน้า Home")
-elif page == "KNN":
-    st.markdown("[ไป KNN](KNN)")
-elif page == "Random Forest":
-    st.markdown("[ไป Random Forest](DForest)")
-elif page == "Naive Bayes":
-    st.markdown("[ไป Naive Bayes](Bay)")
+if model_choice == "KNN":
+    run_knn()
+elif model_choice == "Random Forest":
+    run_dforest()
+elif model_choice == "Naive Bayes":
+    run_bay()
