ad35744d0df09d30f03683184458ec708d907cab
CoffinWingS <156872580+CoffinWingS@users.noreply.github.com>
2025-09-26 04:58:21 +0700

    Update Bay.py

--- pages/Bay.py
+++ pages/Bay.py
@@ -5,6 +5,7 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
+import seaborn as sns  # เพิ่มตรงนี้
 
 def run_bayes():
     st.header("🧠 Naive Bayes for Classification 🧠")
@@ -13,21 +14,20 @@ def run_bayes():
     df = pd.read_csv("./data/shopping.csv")
     st.write(df.head(10))
 
-     # =========================
+    # =========================
     # Boxplot Explorer
     # =========================
     st.subheader("📊 Boxplot Explorer")
-    box_columns = ['Age', 'Size', 'Season', 'Location', 'Shipping Type', 'Item Purchased']
+    box_columns = ['Age', 'Size', 'Season', 'Purchase Amount', 'Location', 'Shipping Type', 'Item Purchased']
     selected_column = st.selectbox("เลือกตัวแปรเพื่อดู Boxplot", box_columns)
 
     plt.figure(figsize=(10,6))
-    if selected_column in ['Age', 'Size', 'Season']:
+    if selected_column in ['Age', 'Size', 'Season', 'Purchase Amount']:
         sns.boxplot(y=df[selected_column])
     else:
         sns.boxplot(x=df[selected_column].astype('category').cat.codes)
     plt.xticks(rotation=45)
     st.pyplot(plt)
-
 
     # เตรียม Features และ Target
     X = df.drop('Category', axis=1)
