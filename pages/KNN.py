6e4e21fa7bececc1a25965c0e13f727450220fd7
CoffinWingS <156872580+CoffinWingS@users.noreply.github.com>
2025-09-26 04:45:51 +0700

    Update KNN.py

--- pages/KNN.py
+++ pages/KNN.py
@@ -88,11 +88,11 @@ def run_knn():
     # Boxplot Explorer
     # =========================
     st.subheader("📊 Boxplot Explorer")
-    box_columns = ['Age', 'Size', 'Season', 'Purchase Amount', 'Location', 'Shipping Type', 'Item Purchased']
+    box_columns = ['Age', 'Size', 'Season', 'Location', 'Shipping Type', 'Item Purchased']
     selected_column = st.selectbox("เลือกตัวแปรเพื่อดู Boxplot", box_columns)
 
     plt.figure(figsize=(10,6))
-    if selected_column in ['Age', 'Size', 'Season', 'Purchase Amount']:
+    if selected_column in ['Age', 'Size', 'Season']:
         sns.boxplot(y=df[selected_column])
     else:
         sns.boxplot(x=df[selected_column].astype('category').cat.codes)
