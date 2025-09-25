f0769b5495e3a101a50b31177c159b1cb07492d4
CoffinWingS <156872580+CoffinWingS@users.noreply.github.com>
2025-09-26 04:55:27 +0700

    Update DForest.py

--- pages/DForest.py
+++ pages/DForest.py
@@ -14,20 +14,6 @@ def run_dforest():
     df = pd.read_csv("./data/shopping.csv")
     st.write(df.head(10))
 
-    # =========================
-    # Boxplot Explorer
-    # =========================
-    st.subheader("📊 Boxplot Explorer")
-    box_columns = ['Age', 'Size', 'Season', 'Location', 'Shipping Type', 'Item Purchased']
-    selected_column = st.selectbox("เลือกตัวแปรเพื่อดู Boxplot", box_columns)
-
-    plt.figure(figsize=(10,6))
-    if selected_column in ['Age', 'Size', 'Season']:
-        sns.boxplot(y=df[selected_column])
-    else:
-        sns.boxplot(x=df[selected_column].astype('category').cat.codes)
-    plt.xticks(rotation=45)
-
     # เตรียม Features และ Target
     X = df.drop('Category', axis=1)
     y = df['Category']
@@ -97,5 +83,17 @@ def run_dforest():
     disp.plot(ax=ax, cmap="Blues", colorbar=False)
     st.pyplot(fig)
 
-    
+    # =========================
+    # Boxplot Explorer
+    # =========================
+    st.subheader("📊 Boxplot Explorer")
+    box_columns = ['Age', 'Size', 'Season', 'Location', 'Shipping Type', 'Item Purchased']
+    selected_column = st.selectbox("เลือกตัวแปรเพื่อดู Boxplot", box_columns)
+
+    plt.figure(figsize=(10,6))
+    if selected_column in ['Age', 'Size', 'Season']:
+        sns.boxplot(y=df[selected_column])
+    else:
+        sns.boxplot(x=df[selected_column].astype('category').cat.codes)
+    plt.xticks(rotation=45)
     st.pyplot(plt)
