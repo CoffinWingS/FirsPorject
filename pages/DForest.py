9da89af527cf8b96016362ccb429148515ed006b
CoffinWingS <156872580+CoffinWingS@users.noreply.github.com>
2025-09-26 04:52:09 +0700

    Update DForest.py

--- pages/DForest.py
+++ pages/DForest.py
@@ -5,6 +5,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 def run_dforest():
     st.header("🧥 Random Forest for Classification 🧥")
@@ -12,7 +13,6 @@ def run_dforest():
     # โหลดข้อมูล
     df = pd.read_csv("./data/shopping.csv")
     st.write(df.head(10))
-
 
     # เตรียม Features และ Target
     X = df.drop('Category', axis=1)
@@ -81,20 +81,19 @@ def run_dforest():
     fig, ax = plt.subplots(figsize=(6,4))
     disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=rf.classes_)
     disp.plot(ax=ax, cmap="Blues", colorbar=False)
+    st.pyplot(fig)
 
- # =========================
+    # =========================
     # Boxplot Explorer
     # =========================
     st.subheader("📊 Boxplot Explorer")
-    box_columns = ['Age', 'Size', 'Season', 'Location', 'Shipping Type', 'Item Purchased']
+    box_columns = ['Age', 'Size', 'Season', 'Purchase Amount', 'Location', 'Shipping Type', 'Item Purchased']
     selected_column = st.selectbox("เลือกตัวแปรเพื่อดู Boxplot", box_columns)
 
     plt.figure(figsize=(10,6))
-    if selected_column in ['Age', 'Size', 'Season']:
+    if selected_column in ['Age', 'Size', 'Season', 'Purchase Amount']:
         sns.boxplot(y=df[selected_column])
     else:
         sns.boxplot(x=df[selected_column].astype('category').cat.codes)
     plt.xticks(rotation=45)
     st.pyplot(plt)
-
-    st.pyplot(fig)
