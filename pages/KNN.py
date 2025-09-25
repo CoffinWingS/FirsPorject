e75ec8182d26c09ed4ccfb0adeb4a9078c6b69dd
CoffinWingS <156872580+CoffinWingS@users.noreply.github.com>
2025-09-26 02:05:35 +0700

    Update KNN.py

--- pages/KNN.py
+++ pages/KNN.py
@@ -23,7 +23,7 @@ X = pd.get_dummies(X)
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=200)
 
 # ====== สร้างและเทรนโมเดล KNN ======
-ModelKNN = KNeighborsClassifier(n_neighbors=5)   # ค่า k = 5 (สามารถปรับได้)
+ModelKNN = KNeighborsClassifier(n_neighbors=3)   # ค่า k = 5 (สามารถปรับได้)
 knn = ModelKNN.fit(x_train, y_train)
 
 # ====== Input จากผู้ใช้ ======
