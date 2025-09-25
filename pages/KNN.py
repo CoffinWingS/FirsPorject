f2995acd1b7df551c856a1efcc15864cb13c5d0f
CoffinWingS <156872580+CoffinWingS@users.noreply.github.com>
2025-09-26 03:22:10 +0700

    Update KNN.py

--- pages/KNN.py
+++ pages/KNN.py
@@ -12,6 +12,10 @@ st.header("🎩K-Nearest Neighbors (KNN) for Classification🎩")
 df = pd.read_csv("./data/shopping.csv")
 st.write(df.head(10))
 
+def run_knn():
+    import streamlit as st
+    st.header("KNN Prediction")
+
 # ====== เตรียม Features และ Target ======
 X = df.drop('Category', axis=1)
 y = df['Category']
