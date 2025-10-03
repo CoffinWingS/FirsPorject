4b21fcf3918591df827ca1d19ffb81786c8ece33
CoffinWingS <156872580+CoffinWingS@users.noreply.github.com>
2025-10-03 14:40:12 +0700

    Update DForest.py

--- pages/DForest.py
+++ pages/DForest.py
@@ -13,13 +13,11 @@ def run_dforest():
     # =========================
     # แสดงรูปภาพคู่กัน
     # =========================
-    # แสดงรูปคู่กัน
     col1, col2 = st.columns(2)
-with col1:
-    st.image("./img/shirt.jpg", caption="Shirt Example", use_container_width=True)
-with col2:
-    st.image("./img/sell.jpg", caption="Sales Promotion", use_container_width=True)
-
+    with col1:
+        st.image("./img/shirt.jpg", caption="Shirt Example", use_container_width=True)
+    with col2:
+        st.image("./img/sell.jpg", caption="Sales Promotion", use_container_width=True)
 
     # โหลดข้อมูล
     df = pd.read_csv("./data/shopping.csv")
